3a73fa6bfd60b35bb5c3874c5e34ab52288544ab
davidportlouis <dportlouis@gmai.com>
2020-12-22 15:50:40 +0530

    added CrossEntropyLoss with Softmax

--- fair/loss.py
+++ fair/loss.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import ndarray
-from fair.utils.np_utils import assert_same_shape
+from fair.utils.np_utils import assert_same_shape, softmax
 
 class Loss:
 
@@ -52,3 +52,25 @@ class MSE(Loss):
 
         return 2.0 * (self.prediction - self.target) / self.prediction.shape[0]
         
+class SCE(Loss):
+
+
+    def __init__(self, eps: float = 1e-9):
+        
+        super().__init__()
+        self.eps = eps
+        self.single_output = False
+
+
+    def _output(self) -> float:
+
+        softmax_preds = softmax(self.prediction, axis=1)
+        # to prevent numeric instability
+        self.softmax_preds = np.clip(softmax_preds, self.eps, 1-self.eps)
+        loss = - (self.target * np.log(self.softmax_preds) + (1 - self.target) * np.log(1 - self.softmax_preds))
+
+        return np.sum(loss)
+
+    def _input_grad(self) -> ndarray:
+
+        return self.softmax_preds - self.target 
